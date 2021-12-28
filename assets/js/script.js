3c7e938c14909f5eb183f8d1e1f9e07b0f072d93
Faris-07 <farisuddin03@gmail.com>
2021-12-28 15:01:08 +0000

    Add variables for javascript

---
+++ assets/js/script.js
@@ -0,0 +1,79 @@
+const selectionButtons = document.querySelectorAll('[data-selection]')
+const computerScoreSpan = document.querySelector('[data-computer-score]')
+const playerScoreSpan = document.querySelector('[data-player-score]')
+const SELECTIONS = [  // Is an array of all the selecetions the user can choose
+  {
+    name: 'rock',
+    beats: 'scissors'
+  },
+  {
+    name: 'paper',
+    beats: 'rock'
+  },
+  {
+    name: 'scissors',
+    beats: 'paper'
+  }
+]
+
+/* Add Event listener to the selection buttons
+   Then loops through all the different sections and finds the one that has the same values as the selectionName variable.
+*/
+selectionButtons.forEach(selectionButton => {
+  selectionButton.addEventListener('click', e => {
+    const selectionName = selectionButton.dataset.selection
+    const selection = SELECTIONS.find(selection => selection.name === selectionName)
+    makeSelection(selection)
+  })
+})
+
+function makeSelection(selection) {
+  const computerSelection = randomSelection()
+  const playerWinner = isWinner(selection, computerSelection)
+  const computerWinner = isWinner(computerSelection, selection)
+
+  if (playerWinner) {
+    incrementScore(playerScoreSpan);
+  } else if (computerWinner){
+    incrementScore(computerScoreSpan);
+  } else {
+    
+  }
+    
+
+  let message = document.getElementById('message');
+  if (playerWinner){
+    message.innerHTML = "You won this round!"
+} else if (computerWinner){
+    message.innerHTML = "You lost this round";
+} else {
+    message.innerHTML = "It's a draw!";
+}
+}
+
+function showChoice(playerWinner, computerSelection) {
+  if (playerWinner){
+    show-choice.innerHTML
+  }
+
+}
+/* 
+   The incrementScore function increases the scoreSpan innerText by 1.
+*/
+function incrementScore(scoreSpan) {
+  scoreSpan.innerText = parseInt(scoreSpan.innerText) + 1
+}
+/* 
+   The isWinner function checks if the selection.beats object is equal to the opponents selection.
+*/
+function isWinner(selection, opponentSelection) {
+  return selection.beats === opponentSelection.name
+}
+
+/* 
+   The random selection function selects a random index between 0 - 2 for the computers selection.
+*/
+function randomSelection() {
+  const randomIndex = Math.floor(Math.random() * SELECTIONS.length)
+  return SELECTIONS[randomIndex]
+}
